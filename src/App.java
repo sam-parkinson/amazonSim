0252e570257344115391d946fac5c2585ef86f89
Sam Parkinson <parkinsonsp42@gmail.com>
2021-07-29 18:34:31 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import org.newdawn.slick.BasicGame;
+
+public class App extends BasicGame {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
