0a9fed14692cf608c0e5a393c0636615c6d74c36
Sam Parkinson <43306113+sam-parkinson@users.noreply.github.com>
2021-08-08 14:22:10 -0400

    Add flag display function to buildings

--- src/Building.java
+++ src/Building.java
@@ -1,14 +1,45 @@
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+/**
+ * 
+ * The Building abstract class defines a building on the map and specifies methods to be implemented by its
+ * child classes.
+ *
+ */
+
 public abstract class Building
 {
-	// how to implement this?
-	private enum Status {INACTIVE, NEEDED, PROGRESS, COMPLETED} // potentially expand with statuses knowing their images?
+	/**
+	 * 
+	 * The Status enum lists out the possible delivery statuses of a house: inactive, delivery needed, delivery
+	 * in progress, and delivery recently completed.
+	 *
+	 */
 	
-	protected int x;					// the x position of the top left corner;
-	protected int y;					// the y position of the top left corner;
+	private enum Status
+	{
+		INACTIVE ("Sprites/flags/inactiveFlag.png"), 
+		NEEDED ("Sprites/flags/neededFlag.png"), 
+		PROGRESS ("Sprites/flags/progressFlag.png"), 
+		COMPLETED ("Sprites/flags/completeFlag.png");
+		
+		private Image flag;
+		Status(String imageLocation)
+		{
+			try {
+				this.flag = new Image(imageLocation);
+			} catch (SlickException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	} 
+	
+	protected int x;				// the x position of the top left corner;
+	protected int y;				// the y position of the top left corner;
 	private Image image;			// all buildings must have a picture indicating which building they are
+	private Status status;			// whether or not the building wants a package
 	
 	/*
 	 * Do the below characteristics need to be added? If so, how? Static variables in subclasses?
@@ -17,7 +48,6 @@ public abstract class Building
 	private int deliveryTime;		// how long it takes for a package to be delivered (what unit?)
 	private int multiplier;			// used in score function, how much score is multiplied
 	private int packagesDelivered;	// how many packages are dropped off at once
-	private Status status;	// whether or not the building wants a package
 	
 	/**
 	 * The Building constructor takes the location of the image depicting the building and creates
@@ -36,15 +66,38 @@ public abstract class Building
 	}
 	
 	/**
+	 * The getX method
+	 * @return The x coordinate of the top-left corner of the Building
+	 */
+	
+	public int getX()
+	{
+		return this.x;
+	}
+	
+	/**
+	 * The getY method
+	 * @return The y coordinate of the top-left corner of the Building
+	 */
+	
+	public int getY()
+	{
+		return this.y;
+	}
+	
+	/**
 	 * The drawBuilding method calls the draw method of the Building's Image provided by the Slick
 	 * library using the x and y coordinates provided when the Building was defined.
 	 */
 	
-	// Getter for coordinates?
 	
 	public void drawBuilding()
 	{
 		image.draw(x,y);
+		if (status.flag != null)
+		{
+			status.flag.draw(x - 16 + (image.getWidth()/2), y - 32);
+		}
 	}
 	
 	/**
@@ -57,6 +110,8 @@ public abstract class Building
 	public void updateDeliveryStatus(Status newStatus)
 	{
 		// TODO: write this function
+		this.status = newStatus;
+		// call function to display flag
 	}
 	
 	
