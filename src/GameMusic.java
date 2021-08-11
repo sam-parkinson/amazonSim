3377a3d6560d30cc9f9f36b7b07726e3f257a413
Nata <natalie.storozhuk@outlook.com>
2021-08-10 23:39:12 -0400

    added comments to Sound and Music classes

--- src/GameMusic.java
+++ src/GameMusic.java
@@ -3,13 +3,19 @@ import org.newdawn.slick.SlickException;
 
 
 public class GameMusic {
-
+	/*
+	 * The menuMusic method play specific music when the menu opens up.
+	 * @return returns menu music.
+	 * */
 	public static Music menuMusic() throws SlickException {
 	
 		return new Music ("sounds/background1.wav");
     }
 	
-
+	/*
+	 * The mapMusic method play specific music when the map opens up.
+	 * @return returns map music.
+	 * */
 	public static Music mapMusic() throws SlickException {
 		
 		return new Music ("sounds/background2.wav");
--- src/GameSounds.java
+++ src/GameSounds.java
@@ -3,25 +3,37 @@ import org.newdawn.slick.Sound;
 
 public class GameSounds {
 
-
+/*
+ * The collisionSound method plays specific sound when collision occurs.
+ * @return returns collision sound.
+ * */
 	public static  Sound collisionSound() throws SlickException {
 		
 		return new Sound("sounds/collision.wav");
 	}
 	
-	
+	/*
+	 * The packagedroppedSound method plays specific sound when a package will be dropped.
+	 * @return returns sound when the package dropped.
+	 * */	
 	public static  Sound packagedroppedSound() throws SlickException {
 		
 		return new Sound("sounds/packagedropped.wav");
 	}
 	
-	
+	/*
+	 * The pointRewardSound method plays specific sound when a point earned.
+	 * @return returns sound when a point earned.
+	 * */
 	public static  Sound pointRewardSound() throws SlickException {
 		
 		return new Sound("sounds/pointReward.wav");
 	}
 	
-	
+	/*
+	 * The gameOverSound method plays specific sound when the game is over.
+	 * @return returns sound when the game is over.
+	 * */
 	public static  Sound gameOverSound() throws SlickException {
 		
 		return new Sound("sounds/gover.wav");
