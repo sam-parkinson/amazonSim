450fda3b0337ea89532ad6582532ec12cd7d081c
Nata <natalie.storozhuk@outlook.com>
2021-08-15 00:11:56 -0400

    added game over sound

--- src/GameOver.java
+++ src/GameOver.java
@@ -1,21 +1,20 @@
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.state.transition.FadeInTransition;
-import org.newdawn.slick.state.transition.FadeOutTransition;
 
 
+/**
+ * The GameOver class defines the game state.
+ */
 
 public class GameOver extends BasicGameState
 {
 
-	private Sound sound;
+	
 
 	public GameOver(int menu) {
 		
@@ -24,30 +23,37 @@ public class GameOver extends BasicGameState
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException
 	{
-		sound = new Sound("sounds/gover.wav");
 		
 		       }
+
+	/**
+	 * The render method draw text and image on the screen
+	 */
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g) throws SlickException
 	{
-      g.drawString("Game Over!", 600, 340);
-	}
+	  Image gameOver = new Image("sprites/gameOver.png");
+	  g.drawImage(gameOver,10,10);
+      g.drawString("Game Over!", 600, 75);
+      g.setColor(Color.red);
+         
+           
+			}
 		
 
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int arg2) throws SlickException
 	{
-//		if(container.getInput().isKeyPressed(Input.KEY_S))
-//			sbg.enterState(1, new FadeOutTransition(), new FadeInTransition());
-//		
-//		else if(container.getInput().isKeyPressed(Input.KEY_E))
-//			sbg.enterState(3);
-		if(container.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			sound.play();
-		}
+		
+		GameSounds.gameOverSound().play();	
+		   
 	}
 
+	/**
+	 * The getID method represents the state ID.
+	 * @return returns state's ID
+	 */
 	@Override
 	public int getID()
 	{
